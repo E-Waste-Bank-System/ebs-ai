c7eba7d1dc20a734ae4bc2a15c37f131991897b3
aldol <aldo0lase@gmail.com>
2025-07-21 19:17:42 +0700

    modified:   src/models/price_predictor.py

--- src/models/price_predictor.py
+++ src/models/price_predictor.py
@@ -18,6 +18,21 @@ from src.utils.mappings import PRICE_CATEGORIES, is_valid_price_category
 
 logger = logging.getLogger(__name__)
 
+class Regresih:
+    def __init__(self, enc1, enc2, model, y_scaler):
+        self.enc1     = enc1
+        self.enc2     = enc2
+        self.model    = model
+        self.y_scaler = y_scaler
+
+    def predict(self, X_df):
+        # 1) encode features exactly as you did manually
+        X_new = X_df.copy()
+        X_new["Kondisi"]    = self.enc1 .transform(X_new[["Kondisi"]])
+        X_new["Nama Item"]  = self.enc2 .transform(X_new["Nama Item"])
+        # 2) predict on your already-trained model
+        y_scaled = self.model.predict(X_new)
+        # 3) inverse-scale to original units
 
 class PricePredictor:
     """
@@ -232,19 +247,3 @@ class PricePredictor:
             "output": "Price estimates in IDR",
             "fallback_available": True
         }
-
-class Regresih:
-    def __init__(self, enc1, enc2, model, y_scaler):
-        self.enc1     = enc1
-        self.enc2     = enc2
-        self.model    = model
-        self.y_scaler = y_scaler
-
-    def predict(self, X_df):
-        # 1) encode features exactly as you did manually
-        X_new = X_df.copy()
-        X_new["Kondisi"]    = self.enc1 .transform(X_new[["Kondisi"]])
-        X_new["Nama Item"]  = self.enc2 .transform(X_new["Nama Item"])
-        # 2) predict on your already-trained model
-        y_scaled = self.model.predict(X_new)
-        # 3) inverse-scale to original units
